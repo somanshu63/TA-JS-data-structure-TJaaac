c10347954baa50ffd885d51f3e6e0c8f90f501a2
somanshu63 <somanshu63@gmail.com>
2022-05-19 18:39:58 +0530

    object practice

--- block-BJaacc/code/index.js
+++ block-BJaacc/code/index.js
@@ -14,7 +14,7 @@ students.batch = 16;
 console.log(students.batch);
 
 // 6. Add another key in the object with the value of the variable `isAdult`. The value of the key should be `true/false`
-students.isAdult = true/false;
+students.isAdult = true;
 
 // 7. Using `console.log` print the value of `isAdult` key from the `students` object
 console.log(students.isAdult);
@@ -26,10 +26,10 @@ students[42] = "the answer to the meaning of life";
 console.log(students[42]);
 
 // 10. Check the length of object named `students`.
-students;
+console.log(students.length);
 
 // 11. Can you define a key of `let or var` in any object? Reason.
-// No
+students["let"] = "any value";
 
 // 12. Delete the `isAdult` property from the object `students`. Also, print the object before or after deleting the property
 console.log(students);
@@ -38,3 +38,4 @@ console.log(students);
 
 // 13. Update the value of the key batch in the object `students` and print it using `console.log`
 students.batch = 18;
+console.log(students.batch);
